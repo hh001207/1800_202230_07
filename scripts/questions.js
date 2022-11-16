7f7941fe89b44d6df27f76ea10793921b7a05b19
MikeSusut <msusut@my.bcit.ca>
2022-11-15 17:45:42 -0800

    There's a lot going on here, muiltple days of work of messing up and trying elsewhere. Started adding the code questions.js to record the user inputs and added to main the points for every hobby to user. This point system will be added to by questions using questions.js which will be a focus of next sprint. TL:DR when you log in, you get variables for each hobby initialied to 0 in db, way to add points WIP.

---
+++ scripts/questions.js
@@ -0,0 +1,13 @@
+function getButton(question) {
+
+var radios = document.getElementsByName("inlineRadioOptions");
+
+for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+    // do whatever you want with the checked radio
+    console.log()
+    q1, radios[i].value;
+    break; }
+  }
+}
+
