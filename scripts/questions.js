3c49ad99b8609fefea2f4925c71a1970e5f266b4
MikeSusut <msusut@my.bcit.ca>
2022-11-17 09:21:38 -0800

    Question 1 result adds to db the rest are on their way

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,14 +1,12 @@
-
 //Gets the result of the radio button
 function getButton() {
+  var radios = document.getElementsByName("inlineRadioOptions");
 
-var radios = document.getElementsByName("inlineRadioOptions");
-
-for (var i = 0, length = radios.length; i < length; i++) {
-  if (radios[i].checked) {
-    // do whatever you want with the checked radio
-    return radios[i].value - 3;
-  }
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      // do whatever you want with the checked radio
+      return radios[i].value - 3;
+    }
   }
   return 0;
 }
@@ -22,20 +20,50 @@ for (var i = 0, length = radios.length; i < length; i++) {
 function addPoints(question) {
   var inp = getButton();
   console.log(inp);
-  switch(question) {
+  switch (question) {
     case 1:
-        if (inp > 0) {
-          db.collection("users").doc(currentUser.id).update({
-            Archery: firebase.firestore.FieldValue.increment(inp)
+      if (inp > 0) {
+        db.collection("users")
+          .doc(currentUser.id)
+          .update({
+            Archery: firebase.firestore.FieldValue.increment(inp),
+            photography: firebase.firestore.FieldValue.increment(inp),
+            soccer: firebase.firestore.FieldValue.increment(inp),
+            hiking: firebase.firestore.FieldValue.increment(inp),
+            Kayak: firebase.firestore.FieldValue.increment(inp),
+          });
+      }
+      if (inp < 0) {
+        inp = inp * -1;
+        db.collection("users").doc(currentUser.id).update({
+          rubixCube: firebase.firestore.FieldValue.increment(inp),
+          coding: firebase.firestore.FieldValue.increment(inp),
+          gym: firebase.firestore.FieldValue.increment(inp),
+          guitar: firebase.firestore.FieldValue.increment(inp),
+          bouldering: firebase.firestore.FieldValue.increment(inp),
+          magic: firebase.firestore.FieldValue.increment(inp),
+          martialArts: firebase.firestore.FieldValue.increment(inp),
+          crochet: firebase.firestore.FieldValue.increment(inp),
+          boardGames: firebase.firestore.FieldValue.increment(inp),
+          basketball: firebase.firestore.FieldValue.increment(inp),
+          suduko: firebase.firestore.FieldValue.increment(inp),
+          magicTricks: firebase.firestore.FieldValue.increment(inp),
+          coincollecting: firebase.firestore.FieldValue.increment(inp),
+          tableTennis: firebase.firestore.FieldValue.increment(inp),
+          origami: firebase.firestore.FieldValue.increment(inp),
         })
-          
-        }
+      }
+      break;
     case 2:
+      if (inp > 0) {
+        db.collection("users").doc(currentUser.id).update({
+          
+      })
 
     case 3:
 
     case 4:
-  
+
     case 5:
 
     case 6:
@@ -47,6 +75,5 @@ function addPoints(question) {
     case 9:
 
     case 10:
-
   }
 }
