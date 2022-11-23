3af4130f471d7afcd7f68b59ab7c496cec29dd77
Max Ryou <heeho3464@gmail.com>
2022-11-22 16:48:21 -0800

    deleted get detail buttons on result page.

--- scripts/result.js
+++ scripts/result.js
@@ -1,9 +1,14 @@
 let change = 4;
 
+let highest;
+
+// if (user)
+
+
 function changelim() {
   change += 4;
   resultCards("hobbies");
-  if (change > 5) {
+  if (change > 4) {
     change = 4;
   }
 }
@@ -34,9 +39,43 @@ function resultCards(collection) {
               //attach to gallery
               document.getElementById(collection + "-go-here").appendChild(newcard);
               //i++;   //if you want to use commented out section
+
           })
+          readDetails(); 
       })
 }
 
 resultCards("hobbies");
 loadSkeleton();
+
+
+function readDetails() {
+  db.collection("hobbies").limit(change).get()                                                     //name of the collection and documents should matach
+    .then(snap => {
+        //var i = 1;  //if you want to use commented out section
+        snap.forEach(doc => { //iterate thru each doc
+            var title = doc.data().name; // get value of the "name" key
+            var details = doc.data().details; // get value of the "details" key
+            document.getElementById("name-goes-here").innerHTML = title;
+            document.getElementById("details-goes-here").innerHTML = details;
+            console.log(title);
+            console.log(details);
+            //update title and text and image
+            //i++;   //if you want to use commented out section
+
+    // .onSnapshot(somedoc => {             
+      
+    //   //arrow notation
+    //      console.log("current document data: " + somedoc.data());                          //.data() returns data object
+    //      document.getElementById("quote-goes-here").innerHTML = somedoc.data().quote;      //using javascript to display the data on the right place
+         
+    //      //Here are other ways to access key:value data fields
+    //      //$('#quote-goes-here').text(tuesdayDoc.data().quote);                                       //using jquery object dot notation
+    //      //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);                                    //using json object indexing
+      })
+  })
+}
+
+readDetails();
+
+       //calling the function
